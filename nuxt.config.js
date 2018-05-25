34f27d2e960bbc2bdcf13d8add5182b2eb95d36f
Mario Noble <mario@contextmultimedia.com>
2018-05-25 12:13:42 -0700

    added back title and json changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'GAAD',
+    title: 'todoee',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Global Accessibility Awareness Day' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -25,6 +25,7 @@ module.exports = {
   /*
   ** Build configuration
   */
+
   build: {
     /*
     ** Run ESLint on save
